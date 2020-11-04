e94a0dc37a768bc9fcef362b77d0d86c1ad8177e
1104lizhe <1615519606@qq.com>
2020-11-04 18:35:44 +0800

    allure集成

--- run.py
+++ run.py
@@ -4,8 +4,5 @@ import pytest, os, allure
 if __name__ == "__main__":
     # os.system('cd C:\Users\ly\PycharmProjects\api_new_git')
     # al = r'pytest C:/Program Files (x86)/Jenkins/workspace/new_api/testcase/ --alluredir C:/Program Files (x86)/Jenkins/workspace/new_api/allure-report --clean-alluredir'
-    pt = r'pytest testcase --alluredir C:\Users\ly\PycharmProjects\api_new_git\report --clean-alluredir'
-    al = r'allure generate report/  --clean'
+    pt = r'pytest C:\Users\ly\PycharmProjects\api_new_git\testcase\ --alluredir report --clean-alluredir'
     os.system(pt)
-    os.system(al)
-    print(pt)
